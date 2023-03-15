1cc149ed61a328a467574b51e2fc2df3e66e1fa4
Himanshu190397 <62283530+Himanshu190397@users.noreply.github.com>
2023-03-16 01:03:20 +0530

    Add run.py and db.py
    Add run.py and db.py with products and product templates.

--- db.py
+++ db.py
@@ -6,10 +6,15 @@ class Database:
     def __init__(self, path):
         self.conn = sqlite3.connect(path)
 
-    def select(self, sql, parameters=[]):
+    def selectAll(self, sql, parameters=[]):
         c = self.conn.cursor()
         c.execute(sql, parameters)
         return c.fetchall()
+
+    def select(self, sql, parameters=[]):
+        c = self.conn.cursor()
+        c.execute(sql, parameters)
+        return c.fetchone()
 
     def execute(self, sql, parameters=[]):
         c = self.conn.cursor()
@@ -18,3 +23,168 @@ class Database:
 
     def close(self):
         self.conn.close()
+
+    def get_beds(self):
+        beds = self.selectAll(
+            'select * from beds order by bid asc'
+        )
+
+        return [{
+            'bid': bed[0],
+            'name': bed[1],
+            'price': bed[2],
+            'user_rating': bed[3],
+            'size': bed[4],
+            'image': bed[5],
+            'available': bed[6]
+        } for bed in beds]
+
+    def get_wardrobes(self):
+        wardrobes = self.selectAll(
+            'select * from wardrobes order by wid asc'
+        )
+
+        return [{
+            'wid': wardrobe[0],
+            'name': wardrobe[1],
+            'price': wardrobe[2],
+            'user_rating': wardrobe[3],
+            'category': wardrobe[4],
+            'image': wardrobe[5],
+            'available': wardrobe[6]
+        } for wardrobe in wardrobes]
+
+    def get_outdoors(self):
+        outdoors = self.selectAll(
+            'select * from outdoor order by oid asc'
+        )
+
+        return [{
+            'oid': outdoor[0],
+            'name': outdoor[1],
+            'price': outdoor[2],
+            'user_rating': outdoor[3],
+            'category': outdoor[4],
+            'image': outdoor[5],
+            'available': outdoor[6]
+        } for outdoor in outdoors]
+
+    def get_chairs(self):
+        chairs = self.selectAll(
+            "select * from chairs order by chid asc;"
+        )
+
+        return [{
+            'chid': chair[0],
+            'name': chair[1],
+            'price': chair[2],
+            'user_rating': chair[3],
+            'material': chair[4],
+            'image': chair[5],
+            'available': chair[6]
+        } for chair in chairs]
+
+    def get_desks(self):
+        desks = self.selectAll(
+            "select * from tables_desks order by tid asc;"
+        )
+
+        return [{
+            'tid': desk[0],
+            'name': desk[1],
+            'price': desk[2],
+            'user_rating': desk[3],
+            'material': desk[4],
+            'image': desk[5],
+            'available': desk[6]
+        } for desk in desks]
+
+    def get_sofas(self):
+        sofas = self.selectAll(
+            "select * from sofas order by sid asc;"
+        )
+
+        return [{
+            'sid': sofa[0],
+            'name': sofa[1],
+            'price': sofa[2],
+            'user_rating': sofa[3],
+            'material': sofa[4],
+            'image': sofa[5],
+            'available': sofa[6],
+            'no_seats': sofa[7]
+        } for sofa in sofas]
+
+    def get_bed(self, bid):
+        bed = self.select(f"select * from beds where bid={bid}")
+        return {
+            'bid': bed[0],
+            'name': bed[1],
+            'price': bed[2],
+            'user_rating': bed[3],
+            'size': bed[4],
+            'image': bed[5],
+            'available': bed[6]
+        }
+
+    def get_chair(self, chid):
+        chair = self.select(f"select * from chairs where chid={chid}")
+        return {
+            'chid': chair[0],
+            'name': chair[1],
+            'price': chair[2],
+            'user_rating': chair[3],
+            'material': chair[4],
+            'image': chair[5],
+            'available': chair[6]
+        }
+
+    def get_sofa(self, sid):
+        sofa = self.select(f"select * from sofas where sid={sid}")
+        return {
+            'sid': sofa[0],
+            'name': sofa[1],
+            'price': sofa[2],
+            'user_rating': sofa[3],
+            'material': sofa[4],
+            'image': sofa[5],
+            'available': sofa[6],
+            'no_seats': sofa[7]
+        }
+
+    def get_wardrobe(self, wid):
+        wardrobe = self.select(f"select * from wardrobes where wid={wid}")
+        return {
+            'wid': wardrobe[0],
+            'name': wardrobe[1],
+            'price': wardrobe[2],
+            'user_rating': wardrobe[3],
+            'category': wardrobe[4],
+            'image': wardrobe[5],
+            'available': wardrobe[6]
+        }
+
+    def get_desk(self, tid):
+        desk = self.select(f"select * from tables_desks where tid={tid}")
+        return {
+            'tid': desk[0],
+            'name': desk[1],
+            'price': desk[2],
+            'user_rating': desk[3],
+            'material': desk[4],
+            'image': desk[5],
+            'available': desk[6]
+        }
+
+    def get_outdoor(self, oid):
+        outdoor = self.select(f"select * from outdoor where oid={oid}")
+
+        return {
+            'oid': outdoor[0],
+            'name': outdoor[1],
+            'price': outdoor[2],
+            'user_rating': outdoor[3],
+            'category': outdoor[4],
+            'image': outdoor[5],
+            'available': outdoor[6]
+        }
--- run.py
+++ run.py
@@ -1,11 +1,148 @@
-from flask import render_template, Flask
+from flask import render_template, Flask, g, jsonify
+from db import Database
 
 app = Flask(__name__)
 
+DATABASE_PATH = './dev/furlenco.db'
+
 
 @app.route('/')
-def home():
+def home_template():
     return render_template('home.html')
+
+
+@app.route('/beds')
+def beds_template():
+    beds = get_db().get_beds()
+    return render_template('products/beds/beds.html', beds=beds)
+
+
+@app.route('/bed')
+def bed_template():
+    return render_template('products/beds/bed.html')
+
+
+@app.route('/chairs')
+def chairs_template():
+    chairs = get_db().get_chairs()
+    return render_template('products/chairs/chairs.html', chairs=chairs)
+
+
+@app.route('/chair')
+def chair_template():
+    return render_template('products/chairs/chair.html')
+
+
+@app.route('/sofas')
+def sofas_template():
+    sofas = get_db().get_sofas()
+    return render_template('products/sofas/sofas.html', sofas=sofas)
+
+
+@app.route('/sofa')
+def sofa_template():
+    return render_template('products/sofas/sofa.html')
+
+
+@app.route('/wardrobes')
+def wardrobes_template():
+    wardrobes = get_db().get_wardrobes()
+    return render_template('products/wardrobes/wardrobes.html', wardrobes=wardrobes)
+
+
+@app.route('/wardrobe')
+def wardrobe_template():
+    return render_template('products/wardrobes/wardrobe.html')
+
+
+@app.route('/tables_desks')
+def tables_desks_template():
+    desks = get_db().get_desks()
+    return render_template('products/desks/tables_desks.html', desks=desks)
+
+
+@app.route('/desk')
+def table_desk_template():
+    return render_template('products/desks/table_desk.html')
+
+
+@app.route('/outdoor')
+def outdoors_template():
+    outdoors = get_db().get_outdoors()
+    return render_template('products/outdoor/outdoor.html', outdoors=outdoors)
+
+
+@app.route('/outdoors')
+def outdoor_template():
+    return render_template('products/outdoor/outdoors.html')
+
+
+@app.route('/test')
+def test_template():
+    return render_template('test.html')
+
+
+@app.route('/api/get_beds', methods=['GET'])
+def api_get_beds():
+    # n = request.args.get('n', default=6)
+    beds = get_db().get_beds()
+    return jsonify(beds)
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = Database(DATABASE_PATH)
+    return db
+
+
+@app.teardown_appcontext
+def close_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+@app.route('/api/get_bed/<bid>', methods=['GET'])
+def api_get_bed(bid):
+    # n = request.args.get('n', default=6)
+    bed = get_db().get_bed(bid)
+    return jsonify(bed)
+
+
+@app.route('/api/get_chair/<chid>', methods=['GET'])
+def api_get_chair(chid):
+    # n = request.args.get('n', default=6)
+    chair = get_db().get_chair(chid)
+    return jsonify(chair)
+
+
+@app.route('/api/get_sofa/<sid>', methods=['GET'])
+def api_get_sofa(sid):
+    # n = request.args.get('n', default=6)
+    sofa = get_db().get_sofa(sid)
+    return jsonify(sofa)
+
+
+@app.route('/api/get_wardrobe/<wid>', methods=['GET'])
+def api_get_wardrobe(wid):
+    # n = request.args.get('n', default=6)
+    wardrobe = get_db().get_wardrobe(wid)
+    return jsonify(wardrobe)
+
+
+@app.route('/api/get_desk/<tid>', methods=['GET'])
+def api_get_desk(tid):
+    # n = request.args.get('n', default=6)
+    desk = get_db().get_desk(tid)
+    return jsonify(desk)
+
+
+@app.route('/api/get_outdoor/<oid>', methods=['GET'])
+def api_get_outdoor(oid):
+    # n = request.args.get('n', default=6)
+    outdoor = get_db().get_outdoor(oid)
+    return jsonify(outdoor)
 
 
 if __name__ == '__main__':
